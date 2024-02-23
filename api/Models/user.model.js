7fc98129d247cbab837a3be2e1673dae86962139
Balaji K <balajibala76394@gmail.com>
2024-02-23 15:38:51 +0530

    create user model

---
+++ api/Models/user.model.js
@@ -0,0 +1,25 @@
+import { timeStamp } from "console";
+import { Mongoose } from "mongoose";
+
+const userSchema = new Mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timeStamp: true }
+);
+
+const User = new Mongoose.model("User", userSchema);
+export default User;
